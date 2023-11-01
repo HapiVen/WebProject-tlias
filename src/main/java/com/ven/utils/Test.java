9793ab5d899ba856e10401a775b13694e4787f38
Ven <1299700263@qq.com>
2023-11-01 21:34:54 +0800

    first commit

--- src/main/java/com/ven/utils/Test.java
+++ src/main/java/com/ven/utils/Test.java
@@ -6,7 +6,7 @@ public class Test {
     public static void main(String[] args) {
         int [][] intevals = new int[][] {{8,10},{15,18},{1,3},{2,6}};
         //从小到大
-        Arrays.sort(intevals, (x, y) -> Integer.compare(x[0], y[0]));
+        Arrays.sort(intevals, (x1, y1) -> Integer.compare(x1[0], y1[0]));
         System.out.println(Arrays.deepToString(intevals));
         //从大到小
         Arrays.sort(intevals, (x, y) -> Integer.compare(y[0], x[0]));
